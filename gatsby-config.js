51b1b3904e37fba5246d2e1adcd34b36a7561a67
Sachie Terada <tcams.41215@gmail.com>
2020-01-26 15:52:31 +0900

    fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  pathPrefix: "/portfolio",
+  pathPrefix: `/portfolio`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
